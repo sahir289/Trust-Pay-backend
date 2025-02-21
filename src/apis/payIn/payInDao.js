59309cf567eaf2767deca6a4dc69a503ecfaf38f
Shadow <shadow77278837@gmail.com>
2025-02-21 10:52:07 +0400

    change hardcode table name to constant.tablename

--- src/apis/payIn/payInDao.js
+++ src/apis/payIn/payInDao.js
@@ -1,28 +1,26 @@
-import { columns } from "../../constants/index.js";
+import { columns, tableName } from "../../constants/index.js";
 import { buildInsertQuery, buildSelectQuery, buildUpdateQuery, executeQuery } from "../../utils/db.js";
-import { columns } from "../../constants/index.js";
-const tableName = 'Payin';
 
 export const generatePayInUrlDao = async (data) => {
-    const [sql, params] = buildInsertQuery(tableName, data)
+    const [sql, params] = buildInsertQuery(tableName.PAYIN, data)
     const result = await executeQuery(sql, params);
     return result.rows[0];
 }
 
 export const getPayInUrlDao = async (filters) => {
-    const [sql, params] = buildSelectQuery(`SELECT * FROM "${tableName}" WHERE 1=1`, filters);
+    const [sql, params] = buildSelectQuery(`SELECT * FROM "${tableName.PAYIN}" WHERE 1=1`, filters);
     const result = await executeQuery(sql, params);
     return result.rows[0];
 }
 
 export const getPayInUrlsDao = async (filters = {}) => {
-    const [sql, params] = buildSelectQuery(`SELECT * FROM "${tableName}" WHERE 1=1`, filters);
+    const [sql, params] = buildSelectQuery(`SELECT * FROM "${tableName.PAYIN}" WHERE 1=1`, filters);
     const result = await executeQuery(sql, params);
     return result.rows;
 }
 
 export const updatePayInUrlDao = async (id, data, conn) => {
-    const [sql, params] = buildUpdateQuery(tableName, data, { id });
+    const [sql, params] = buildUpdateQuery(tableName.PAYIN, data, { id });
     if (conn && conn.query) {
         const result = await conn.query(sql, params);
         return result.rows[0];
@@ -38,7 +36,7 @@ export const getPayinsDao = async (
     sortBy,
     sortOrder
 ) => {
-    const baseQuery = `SELECT * FROM "${tableName}" WHERE 1=1`;
+    const baseQuery = `SELECT * FROM "${tableName.PAYIN}" WHERE 1=1`;
     const [sql, queryParams] = buildSelectQuery(baseQuery, search, columns.PAYIN, page, pageSize, sortBy, sortOrder, typeof search != 'string');
     // Execute query
     const result = await executeQuery(sql, queryParams);
