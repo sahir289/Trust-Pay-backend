98d2b00904870c70cbaefb6e07195a0b74e2d763
JoeySang19 <joeysang191@gmail.com>
2025-02-24 05:18:22 -0800

    unused removal

--- src/apis/auth/authController.js
+++ src/apis/auth/authController.js
@@ -1,6 +1,5 @@
 import { logoutSet } from '../../middlewares/auth.js';
-import { INSERT_AUTH_SCHEMA } from '../../schemas/authSchema.js';
-import { BadRequestError, ValidationError } from '../../utils/appErrors.js';
+import { BadRequestError } from '../../utils/appErrors.js';
 // import { verifyToken } from '../../utils/auth.js';
 import { sendSuccess } from '../../utils/responseHandlers.js';
 import { loginService, 
@@ -12,10 +11,10 @@ const loginController = async (req, res) => {
   let clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   const payload = {...req.body, clientIP};
   const options = { abortEarly: false };
-  // const joiValidation = INSERT_AUTH_SCHEMA.validate(payload, options);
-  // if (joiValidation.error) {
-  //   throw new ValidationError(joiValidation.error);
-  // }
+  const joiValidation = INSERT_AUTH_SCHEMA.validate(payload, options);
+  if (joiValidation.error) {
+    throw new ValidationError(joiValidation.error);
+  }
   const data = await loginService(payload);
   res.cookie('refreshToken', data.refreshToken, {
     httpOnly: true,
