b644b5ba481d070f726bef63e8c923a2510705b4
JoeySang19 <joeysang191@gmail.com>
2025-03-06 09:16:37 -0800

    payoutdetails

--- src/apis/payOut/payOutDao.js
+++ src/apis/payOut/payOutDao.js
@@ -23,69 +23,64 @@ export const createPayoutDao = async (conn, data) => {
 export const getPayoutsDao = async (conn, payload) => {
   try {
     let baseQuery = `SELECT DISTINCT ON (u.id)
-  u.id, u.sno,u.user, u.merchant_id, u.bank_acc_id, 
- u.amount, u.status, u.failed_reason, u.currency, 
-  u.merchant_order_id, u.acc_no, u.acc_holder_name, u.ifsc_code,u.bank_name,
-  u.upi_id, utr_id, u.rejected_reason, 
-  u.payout_merchant_commission, u.payout_vendor_commission,
-  u.from_bank_acc_id, 
- u.approved_at, u.created_by, u.updated_by, u.created_at, u.updated_at, u.config AS payout_config,
+    u.id, 
+    u.sno,
+    u.user, 
+    u.merchant_id, 
+    u.bank_acc_id, 
+    u.amount,
+    u.status, 
+    u.failed_reason, 
+    u.currency, 
+    u.merchant_order_id, 
+    u.upi_id, 
+    u.utr_id, 
+    u.rejected_reason, 
+    u.from_bank_acc_id,
+    u.payout_merchant_commission, 
+    u.payout_vendor_commission,
+    u.approved_at, 
+    u.created_by, 
+    u.updated_by, 
+    u.created_at, 
+    u.updated_at, 
+    u.config AS payout_config,
+    
+    v.code AS vendor_code, v.id AS vendor_id, v.user_id AS vendor_user_id,
+    b.id AS bank_table_id, b.user_id, b.nick_name,
+    r.id AS merchant_table_id,
+    
+    json_build_object(
+            'account_holder_name', u.acc_holder_name,
+            'account_no', u.acc_no,
+            'ifsc_code', u.ifsc_code,
+            'bank_name', u.bank_name
+        ) AS user_bank_details,
+         
+    json_build_object(
+            'merchant_code', r.code,
+            'return_url', r.config->>'return_url',
+            'notify_url', r.config->>'notify_url'
+        ) AS merchant_details
   
-  v.code AS vendor_code, v.id AS vendor_id, v.user_id AS vendor_user_id,
-  b.id AS bank_table_id, b.user_id, b.nick_name,
-  r.code AS merchant_code, r.id AS merchant_table_id, r.config
-FROM public."Payout" u
-LEFT JOIN public."Merchant" r 
-  ON u.merchant_id = r.id
-LEFT JOIN public."BankAccount" b
-  ON u.bank_acc_id = b.id
-LEFT JOIN public."Vendor" v 
-  ON v.user_id = b.user_id
-WHERE u.is_obsolete = false AND u.company_id = $1
-ORDER BY u.id
-LIMIT $3 OFFSET $2;
-`;
+  FROM public."Payout" u
+  
+  LEFT JOIN public."Merchant" r 
+    ON u.merchant_id = r.id
+  LEFT JOIN public."BankAccount" b
+    ON u.bank_acc_id = b.id
+  LEFT JOIN public."Vendor" v 
+    ON v.user_id = b.user_id
+  WHERE u.is_obsolete = false AND u.company_id = $1
+  ORDER BY u.id
+  LIMIT $3 OFFSET $2;
+  `;
+  
 
     const queryParams = [payload.company_id, payload.page, payload.limit];
     const result = await conn.query(baseQuery, queryParams);
-    return {
-      totalCount: result.rows.length,
-      rows: result.rows.reduce(
-        (acc, res) =>
-          acc.concat({
-            id: res.id,
-            sno: res.sno,
-            upi_short_code: res.upi_short_code,
-            amount: res.amount,
-            status: res.status,
-            is_notified: res.is_notified,
-            user_submitted_utr: res.user_submitted_utr,
-            merchant_order_id: res.merchant_order_id,
-            user: res.user,
-            bank_account: res.nick_name,
-            merchant: {
-              code: res.merchant_code,
-              ReturnUrl: res.payout_config.return_url,
-              NotifyUrl: res.payout_config.notify_url,
-            },
-            vendor: res.vendor_code,
-            bank_response: {
-              amount: res.bank_res_amount,
-              utr: res.utr,
-            },
-            payout_merchant_commission: res.payout_merchant_commission,
-            payout_vendor_commission: res.payout_vendor_commission,
-            user_submitted_image: res.user_submitted_image,
-            duration: res.duration,
-            approved_at: res.approved_at,
-            created_by: res.created_by,
-            updated_by: res.updated_by,
-            created_at: res.created_at,
-            updated_at: res.updated_at,
-          }),
-        [],
-      ),
-    };
+    return { totalCount: result.rows.length, rows: result.rows };
+
   } catch (error) {
     console.error('Error in createPayoutDao:', error);
     throw error.message;
