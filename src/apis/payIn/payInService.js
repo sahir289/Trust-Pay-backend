47e17e58df49bc5cde236cbeb7eaf3ef24354c77
Shadow <shadow77278837@gmail.com>
2025-04-15 17:44:04 +0400

    Bug Fix:
    If user submitted UTR is not available then don't add UTR while doing check UTR

--- src/apis/payIn/payInService.js
+++ src/apis/payIn/payInService.js
@@ -837,7 +837,7 @@ export const processPayInService = async (conn, payload, updated_by, tele_check)
   });
   const updatePayInData = {
     amount,
-    user_submitted_utr: userSubmittedUtr,
+    user_submitted_utr: payIn?.user_submitted_utr ? userSubmittedUtr : null,
     is_url_expires: true,
     one_time_used: true,
     duration,
