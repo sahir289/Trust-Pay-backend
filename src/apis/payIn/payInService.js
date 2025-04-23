65ee3b1f2165eaa6c2b7ed04232c9552e4c6f030
Shadow <shadow77278837@gmail.com>
2025-04-23 18:23:27 +0400

    Refactor: change the check-payin-status API Response

--- src/apis/payIn/payInService.js
+++ src/apis/payIn/payInService.js
@@ -390,8 +390,17 @@ export const checkPayInStatusService = async (
     id: payInId,
     merchant_order_id: merchantOrderId,
   });
+
   if (!payIn) {
     throw new NotFoundError('payIn not found');
+  }
+
+  let botResponse;
+  if (payIn.bank_response_id) {
+    botResponse = await getBankResponseDao({
+      id: payIn.bank_response_id,
+      company_id: payIn.company_id,
+    });
   }
 
   return {
@@ -399,6 +408,7 @@ export const checkPayInStatusService = async (
     merchantOrderId: payIn.merchant_order_id,
     amount: payIn.amount,
     payinId: payIn.id,
+    utr_id: [Status.INITIATED, Status.ASSIGNED, Status.DROPPED].includes(payIn.status) ? null : botResponse.utr ? botResponse.utr : payIn.user_submitted_utr,
   };
 };
 
