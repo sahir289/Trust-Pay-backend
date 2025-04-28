8c489521963d7f8d25610d5a44d2706ec2d345c5
JoeySang19 <joeysang191@gmail.com>
2025-04-28 09:52:07 -0700

    manual utr added for screenshot upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -320,11 +320,13 @@ export const merchantColumns = {
     'config',
   ],
   USER: [
+    'id', // -- while enabling user id going undefined
     'first_name',
     'last_name',
     'email',
     'contact_no',
     'user_name',
+    'is_enabled',
     'code',
     'config',
   ],
