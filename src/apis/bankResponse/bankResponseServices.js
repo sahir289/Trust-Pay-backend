512a22cf61f279d659a5a462c69770249b667b1c
Luv-83 <luvraghuvanshi83@gmail.com>
2025-05-02 16:57:51 +0400

    add commission when check utr

--- src/apis/bankResponse/bankResponseServices.js
+++ src/apis/bankResponse/bankResponseServices.js
@@ -412,7 +412,7 @@ const getBankResponseService = async (payload, role, page, limit, search) => {
       ...(search ? { search } : {}),
       ...filters,
     }
-    return await getBankResponseDaoAll(filters, page, limit, 'updated_by', null, filterColumns);
+    return await getBankResponseDaoAll(filters, page, limit, 'updated_at', 'DESC', filterColumns);
   } catch (error) {
     logger.error('Error in getBankResponseService:', error);
     throw new InternalServerError(error);
