dd66ed970a6434668d32e983d4dc2132784eb361
aryan11814 <aryan181431@gmail.com>
2025-05-14 22:32:57 +0530

    refact update payout

--- src/apis/payOut/payOutService.js
+++ src/apis/payOut/payOutService.js
@@ -447,7 +447,7 @@ const updatePayoutService = async (conn, ids, payload, role) => {
       conn,
     );
     const merchantArr = await getMerchantsDao({
-      id: singleWithdrawData.merchant_id,
+      id: singleWithdrawData[0].merchant_id,
     });
     const merchant = merchantArr[0];
     if (!merchant) {
