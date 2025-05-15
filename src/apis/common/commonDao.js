223c2d10896bd9f00a6331e3591cca4c930bd361
Shadow <shadow77278837@gmail.com>
2025-05-15 18:19:15 +0400

    console Removed

--- src/apis/common/commonDao.js
+++ src/apis/common/commonDao.js
@@ -82,8 +82,6 @@ export const getTotalCountDao = async (tablename, role, filters, roleIs) => {
       });
     }
 
-    console.log(query, 'query');
-    console.log(params, 'params');
     const result = await executeQuery(query, params);
     return parseInt(result.rows[0].count, 10); // Ensure the count is returned as an integer
   } catch (error) {
