1027fbd9f69dea9f7b232323707472a1fada6fbf
psahir <psahir289@gmail.com>
2025-05-15 20:14:07 +0400

    bug: resolved and refactored authentication flow

--- src/apis/auth/authController.js
+++ src/apis/auth/authController.js
@@ -1,8 +1,10 @@
 import { logoutSet } from '../../middlewares/auth.js';
 import { INSERT_AUTH_SCHEMA } from '../../schemas/authSchema.js';
 import { BadRequestError, ValidationError } from '../../utils/appErrors.js';
+import { generateUserToken, verifyToken } from '../../utils/auth.js';
 // import { verifyToken } from '../../utils/auth.js';
 import { sendSuccess } from '../../utils/responseHandlers.js';
+import { updateSessionDao } from './authDao.js';
 import {
   loginService,
   // logoutService,
@@ -11,7 +13,8 @@ import {
   verificationService,
   verfyUserService,
   verfyOtpService,
-  forgetPasswordService
+  forgetPasswordService,
+  logoutService,
 } from './authService.js';
 
 const loginController = async (req, res) => {
@@ -25,64 +28,80 @@ const loginController = async (req, res) => {
   const data = await loginService(payload, clientIP);
   ///for first login user
   if (data.isLoginFirst) {
-      return sendSuccess(res, data, "user's first login");
+    return sendSuccess(res, data, "user's first login");
   }
-    res.cookie('refreshToken', data.refreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'Strict',
-    });
+  res.cookie('refreshToken', data.refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'Strict',
+  });
   const token = {
     accessToken: data.tokenInfo.accessToken,
     sessionId: data.sessionId,
   };
-  console.log('login successfully', 'info');
   return sendSuccess(res, token, 'login successfully');
 };
 
 const refreshTokenController = async (req, res) => {
   const { refreshToken } = req.cookies;
-  console.log(req.cookies, 'refreshToken');
   if (!refreshToken) {
-    throw new BadRequestError('Unauthorized');
+    throw new BadRequestError('Unauthorized access, Try to login again');
   }
-  const data = await refreshTokenService(refreshToken);
-  return sendSuccess(res, data, 'refresh token generated successfully');
+  const decoded = verifyToken(refreshToken, { ignoreExpiration: true });
+  const session = await refreshTokenService(
+    decoded.user_id,
+    decoded.company_id,
+    refreshToken,
+  );
+  const newAccessToken = generateUserToken(decoded);
+
+  const config = JSON.parse(session.config);
+  config.token.access_token = newAccessToken;
+  await updateSessionDao(
+    decoded.user_id,
+    decoded.company_id,
+    session.session_id,
+    config,
+  );
+  const token = { accessToken: newAccessToken };
+
+  return sendSuccess(res, token, 'Refresh token generated successfully');
 };
 
 const logoutController = async (req, res) => {
   const { session_id } = req.body;
-  console.log(session_id, 'session_id');
   const token = req.header('x-auth-token');
-  // const decodeToken = verifyToken(token);
-  // const data = await logoutService(decodeToken, session_id);
-  // console.log(data, "data")
-  logoutSet.add(token); // we will update this logic in future, currently this approach is not good to invalidate token
-  console.log('logout successfully', 'info');
+  const decodeToken = verifyToken(token);
+  await logoutService(decodeToken, session_id);
+  logoutSet.add(token);
   return sendSuccess(res, {}, 'logout successfully');
 };
 
-const verificationController =async(req, res) => {
+const verificationController = async (req, res) => {
   const { user_name } = req.user;
   const { password } = req.body;
   let ids = {};
-  const validate = await verificationService(ids, { user_name, password })
- if (!validate) {
-        throw new BadRequestError('Invalid password');
- }
+  const validate = await verificationService(ids, { user_name, password });
+  if (!validate) {
+    throw new BadRequestError('Invalid password');
+  }
 
- return sendSuccess(res, {}, 'Verification successful');
-}
+  return sendSuccess(res, {}, 'Verification successful');
+};
 const changePasswordController = async (req, res) => {
-const { user_id,user_name } = req.user;
-const { oldPassword, password } = req.body;
-const changedPassword= await changePasswordService({ user_id, user_name, password, oldPassword });
+  const { user_id, user_name } = req.user;
+  const { oldPassword, password } = req.body;
+  const changedPassword = await changePasswordService({
+    user_id,
+    user_name,
+    password,
+    oldPassword,
+  });
   if (!changedPassword) {
-        throw new BadRequestError('Invalid old password');
+    throw new BadRequestError('Invalid old password');
   }
   return sendSuccess(res, {}, 'Password Changed Successfully');
 };
-
 
 const verfyUserController = async (req, res) => {
   const { user_name } = req.body;
@@ -96,13 +115,13 @@ const verfyOtpController = async (req, res) => {
   const { otp } = req.body;
   const verfyUser = await verfyOtpService(otp);
   if (!verfyUser) {
-    throw new BadRequestError("Invalid OTP");
+    throw new BadRequestError('Invalid OTP');
   }
   return sendSuccess(res, verfyUser, 'Verified Otp Successfully');
 };
 const forgetPasswordController = async (req, res) => {
-  const { password,user_id} = req.body;
-  const verfyUser = await forgetPasswordService({password,user_id});
+  const { password, user_id } = req.body;
+  const verfyUser = await forgetPasswordService({ password, user_id });
   if (!verfyUser) {
     throw new BadRequestError("Invalid User's Info");
   }
@@ -117,5 +136,5 @@ export {
   verificationController,
   verfyUserController,
   verfyOtpController,
-  forgetPasswordController
+  forgetPasswordController,
 };
