ca4c9655bd497cebe195a6e2b77e7879eb4c0aaa
JoeySang19 <joeysang191@gmail.com>
2025-05-15 07:56:58 -0700

    console removed

--- src/apis/common/commonDao.js
+++ src/apis/common/commonDao.js
@@ -85,7 +85,6 @@ export const getTotalCountDao = async (tablename, role, filters, roleIs) => {
     }
 
     const result = await executeQuery(query, params);
-    console.log(query, params, result.rows,  '______________')
     return parseInt(result.rows[0].count, 10); // Ensure the count is returned as an integer
   } catch (error) {
     if (error.code === '42P01') {
