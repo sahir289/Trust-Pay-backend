c94ce552ebf8d49098a6d3e2b7751b474688e662
aryan11814 <aryan181431@gmail.com>
2025-06-13 13:21:23 +0530

    add some changes in payout

--- src/utils/advisoryLock.js
+++ src/utils/advisoryLock.js
@@ -1,29 +1,27 @@
 import { BadRequestError } from './appErrors.js';
 import { logger } from './logger.js';
-
+import { beginTransaction, commit, rollback } from './db.js';
 
 export async function checkLockEdit(conn, id) {
   try {
-    // Convert UUID to integer for locking
+    await beginTransaction(conn);
     const lockKey = parseInt(id.replace(/-/g, ''), 16) % 1000000;
-
-    // Attempt to acquire advisory lock
     const lockResult = await conn.query(
       'SELECT pg_try_advisory_xact_lock($1) AS acquired',
       [lockKey],
     );
-
     if (!lockResult.rows[0].acquired) {
+      await rollback(conn);
       throw new BadRequestError(
         'This record is currently being updated by another user. Please try again later.',
       );
     }
-
-    return { success: true, message: 'Record is available for editing.' };
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await commit(conn);
+    return true
   } catch (error) {
+    await rollback(conn);
     logger.error('Error while attempting to check lock for ID', error);
-    throw error
+    throw error;
   }
 }
-
-
