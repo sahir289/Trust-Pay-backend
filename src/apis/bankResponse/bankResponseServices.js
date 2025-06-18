0628096bf1afe917f59d68f22a6b0eba5dafe215
JoeySang19 <joeysang191@gmail.com>
2025-06-18 10:04:38 +0400

    comment

--- src/apis/bankResponse/bankResponseServices.js
+++ src/apis/bankResponse/bankResponseServices.js
@@ -136,19 +136,19 @@ const createBankResponseService = async (
       ...(isValidAmountCode && { upi_short_code }),
     };
 
-    if (isValidAmountCode) {
-      const isAmountCodeExist = await getBankResponseDao(
-        { upi_short_code, company_id },
-        null,
-        null,
-        null,
-        null,
-        filterColumns,
-      );
-      if (isAmountCodeExist) {
-        return { message: 'Amount code already exist' };
-      }
-    }
+    // if (isValidAmountCode) {
+    //   const isAmountCodeExist = await getBankResponseDao(
+    //     { upi_short_code, company_id },
+    //     null,
+    //     null,
+    //     null,
+    //     null,
+    //     filterColumns,
+    //   );
+    //   if (isAmountCodeExist) {
+    //     return { message: 'Amount code already exist' };
+    //   }
+    // }
 
     const sendNotification = async (status, data) => {
       await notifyNewTableEntry(tableName.BANK_RESPONSE, status, data);
