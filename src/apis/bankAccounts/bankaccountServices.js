fbc2c3b132914f12fb0b9eedbbad6dc30d8f51ed
aryan11814 <aryan181431@gmail.com>
2025-06-27 19:34:43 +0530

    add check for max limit zero in bank

--- src/apis/bankAccounts/bankaccountServices.js
+++ src/apis/bankAccounts/bankaccountServices.js
@@ -220,7 +220,12 @@ const updateBankaccountService = async (
     if (role === Role.VENDOR_OPERATIONS) {
       userId = userHierarchys[0]?.config?.parent;
     }
-    if (Object.keys(payload).length === 1 && payload.latest_balance && bank[0].is_enabled) {
+    if (
+      Object.keys(payload).length === 1 &&
+      payload.latest_balance &&
+      bank[0].is_enabled &&
+      (bank[0].config?.max_limit && bank[0].config?.max_limit !== 0)
+    ) {
       if (payload.latest_balance >= bank[0].config?.max_limit) {
         payload.is_enabled = false;
         deactivateBank(bank[0].nick_name, ids.id, userId);
