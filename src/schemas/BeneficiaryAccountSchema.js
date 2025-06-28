faea845243a5b83a10af14374333e4e82a7f6693
Shadow <shadow77278837@gmail.com>
2025-06-28 17:52:20 +0400

    Bug FIX: Beneficiary Bugs
    1. Beneficiary is nit getting edited or deleted Fixed
    2. Beneficiary update error upi_id not allowed to be empty and accout no must be number Fixed

--- src/schemas/BeneficiaryAccountSchema.js
+++ src/schemas/BeneficiaryAccountSchema.js
@@ -6,7 +6,7 @@ export const BENEFICIARY_ACCOUNT_SCHEMA = Joi.object({
     .label('user_id')
     .allow(null, '')
     .optional(),
-  upi_id: Joi.string().label('upi_id').optional(),
+  upi_id: Joi.string().label('upi_id').allow(null, '').optional(),
   type: Joi.string().allow(null, '').label('type').optional(),
   acc_holder_name: Joi.string().label('acc_holder_name').required(),
   acc_no: Joi.string().label('acc_no').required(),
@@ -17,9 +17,9 @@ export const BENEFICIARY_ACCOUNT_SCHEMA = Joi.object({
 });
 
 export const UPDATE_BENEFICIARY_ACCOUNT_SCHEMA = Joi.object({
-  upi_id: Joi.string().label('upi_id').optional(),
+  upi_id: Joi.string().label('upi_id').allow(null, '').optional(),
   acc_holder_name: Joi.string().label('acc_holder_name').optional(),
-  acc_no: Joi.number().label('acc_no').optional(),
+  acc_no: Joi.string().label('acc_no').optional(),
   ifsc: Joi.string().label('ifsc').optional(),
   bank_name: Joi.string().label('bank_name').optional(),
   config: Joi.object().label('config').optional(),
