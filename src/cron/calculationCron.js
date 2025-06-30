c70ae3a3789c66f1676e1dc7d9654feb99fc1287
Luv-83 <luvraghuvanshi83@gmail.com>
2025-06-30 17:34:57 +0400

    modify time

--- src/cron/calculationCron.js
+++ src/cron/calculationCron.js
@@ -19,7 +19,7 @@ const IST = 'Asia/Kolkata';
 // Only run cron jobs in development environment
 // if (process.env.NODE_ENV == 'production') {
   cron.schedule(
-    '45 6 * * *',
+    '08 19 * * *',
     () => {
       logger.info('Running calculation cron job in production mode');
       collectCalculationData();
