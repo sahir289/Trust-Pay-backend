469ea7adbf3de3c5c9c96bbae6e36455c4afc0ad
Luv-83 <luvraghuvanshi83@gmail.com>
2025-07-31 15:23:55 +0400

    remove unwanted code

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -2,13 +2,8 @@ import Redis from 'ioredis';
 import { logger } from './logger.js'; // Adjust the import path as needed
 
 const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
-const redisPassword = process.env.REDIS_PASSWORD;
 
-const redisOptions = redisPassword
-  ? { password: redisPassword }
-  : {};
-
-const redisClient = new Redis(redisUrl, redisOptions);
+const redisClient = new Redis(redisUrl);
 
 redisClient.on('connect', () => {
   console.log('[Redis] Connected');
