7b0e36077a6b0935985f10cefd31783f0689c6c1
psahir <psahir289@gmail.com>
2025-09-22 20:48:25 +0400

    refactored

--- src/zentechind/zentechInd.js
+++ src/zentechind/zentechInd.js
@@ -1,10 +1,11 @@
 import crypto from 'crypto';
 import axios from 'axios';
 import { logger } from '../utils/logger.js';
+import config from '../config/config.js';
 
 export const generateHash = (body) => {
-  const salt = 'b5167621a04a886983b1a30f710e627e';
-  const collection_id = 'V1050580aeca1';
+  const salt = config.zentechind.salt;
+  const collection_id = config.zentechind.collectionId;
   const { amount, order_id } = body;
   const stringToHash = `${collection_id}|${amount}|${order_id}|${salt}`;
   return crypto.createHash('sha512').update(stringToHash).digest('hex');
@@ -13,7 +14,7 @@ export const generateHash = (body) => {
 export const createZenTechIndTransaction = async (deposit, amount) => {
   try {
     const body = {
-      collection_id: 'V1050580aeca1',
+      collection_id: config.zentechind.collectionId,
       order_id: deposit.merchant_order_id,
       amount,
       user_id: deposit.user,
@@ -21,8 +22,7 @@ export const createZenTechIndTransaction = async (deposit, amount) => {
     const hash = generateHash(body);
     console.log(hash, "hash");
     const requestBody = { ...body, hash };
-    const API_URL =
-      'https://zentechind.com/corporate/api/v1/transaction/create';
+    const API_URL = config.zentechind.url;
     const response = await axios.post(API_URL, requestBody, {
       headers: { 'Content-Type': 'application/json' },
     });
