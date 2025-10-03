ccafb6d4f5e62271414d030c6bbff967c4a386ab
psahir <psahir289@gmail.com>
2025-10-03 17:06:31 +0400

    added build versioning

---
+++ version.js
@@ -0,0 +1,7 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+
+export const getVersion = (req, res) => {
+  res.json({ version: pkg.version });
+};
