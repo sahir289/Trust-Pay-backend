de4eb41f014e0e894a7eb43b1b62f388bab91d51
psahir <psahir289@gmail.com>
2025-10-08 18:46:40 +0400

    refactor: refactor basic status code added

--- src/apis/webhooks/zenTechInd.js
+++ src/apis/webhooks/zenTechInd.js
@@ -9,7 +9,7 @@ import { getBankResponseByUTR } from '../bankResponse/bankResponseDao.js';
 
 export const zenTechIndWebhook = async (req, res) => {
   try {
-    sendSuccess(res, 200, 'Webhook received successfully');
+    sendSuccess(res, {}, 'Webhook received successfully');
     const body = req.body?.transaction;
     const hash = generateHash(body);
     if (hash !== body.hash) {
