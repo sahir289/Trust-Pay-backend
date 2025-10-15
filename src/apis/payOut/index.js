98a63b7639b5afe825ccd96932b0033a48b92511
psahir <psahir289@gmail.com>
2025-10-15 20:14:41 +0400

    merged

--- src/apis/payOut/index.js
+++ src/apis/payOut/index.js
@@ -19,7 +19,6 @@ import { AccessRoles } from '../../constants/index.js';
 import { payAssistTransactionStatusCallback } from '../../callBacksAndWebHook/callBacks/payAsistWebHook.js';
 import { tataPayTransactionStatusCallback } from '../../callBacksAndWebHook/callBacks/tataPayWebHook.js';
 import { getClickrrWalletBalance, initiateClickrrPayout } from '../../clickrr/clickrr.js';
-import { getClickrrWalletBalance, initiateClickrrPayout } from '../../clickrr/clickrr.js';
 const router = express.Router();
 
 /**
