d087a095e0d7c3c343bef2ce1c93b44b207fcee1
Shadow <shadow77278837@gmail.com>
2025-11-27 17:45:24 +0400

    Update clickrr.js

--- src/apis/webhooks/clickrr.js
+++ src/apis/webhooks/clickrr.js
@@ -43,7 +43,7 @@ export const clickrrWebhook = async (req, res) => {
       },
     };
 
-    logger.info('Payout updated from Clickrr webhook:', newPayload);
+    logger.info(`Payout updated from Clickrr webhook for ID ${merchant_order_id}`, newPayload);
     const clickrrResponse = await updatePayoutService(conn, ids, newPayload);
     logger.info('Payout processed:', clickrrResponse);
     await commit(conn);
