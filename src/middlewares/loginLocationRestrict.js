df2b4e9b874aa94528660944a5b497532e70e351
aryan11814 <aryan181431@gmail.com>
2026-02-10 15:58:32 +0400

    add changes for location login

--- src/middlewares/loginLocationRestrict.js
+++ src/middlewares/loginLocationRestrict.js
@@ -50,10 +50,7 @@ const createGeoGuard = (options = {}) => {
     restrictVpnForRoles = ['VENDOR'],
     blockedCountries = [],
     roleRegionRules = {
-      VENDOR: {
-        country: 'United Arab Emirates',
-        blockedRegions: blockedRegions,
-      },
+      VENDOR: { country: 'India', blockedRegions: blockedRegions},
     },
     defaultLocation = { latitude: 0, longitude: 0, accuracy: 1000 }, // Default fallback location
   } = { ...config.geoGuard, ...options };
