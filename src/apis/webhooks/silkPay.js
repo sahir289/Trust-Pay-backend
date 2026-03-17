67e5b203c03a3d327d9a76e3db84547f380605ab
Shadow <shadow77278837@gmail.com>
2026-03-17 11:57:22 +0400

    Bug Fix: Multiple time process on SilkPay Callback

--- src/apis/webhooks/silkPay.js
+++ src/apis/webhooks/silkPay.js
@@ -6,6 +6,7 @@ import { getPayInIntentDao } from '../payIn/payInDao.js';
 import { processPayInWebHookService } from '../payIn/payInService.js';
 import { generateHash } from '../../intent/createIntentTransaction.js';
 import { getBankResponseByUTR } from '../bankResponse/bankResponseDao.js';
+import { Status } from '../../constants/index.js';
 
 const processingSet = new Set();
 
@@ -42,6 +43,11 @@ export const silkPayWebhook = async (req, res) => {
     const payIn = await getPayInIntentDao(merchantOrderId);
 
     const bankResponsePayload = `${body?.amount} nil ${payload.userSubmittedUtr} ${payIn.bank_acc_id}`;
+
+    if (payIn.status === Status.SUCCESS) {
+      logger.warn(`PayIn already marked as SUCCESS for merchantOrderId ${merchantOrderId} - skipping processing`);
+      return;
+    }
 
     const utrAlreadyExist = await getBankResponseByUTR(
       payload.userSubmittedUtr,
