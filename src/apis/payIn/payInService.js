798ab3109fffc2b831364bc194ab407e93ad3147
psahir <psahir289@gmail.com>
2026-04-07 15:58:32 +0400

    fix:

--- src/apis/payIn/payInService.js
+++ src/apis/payIn/payInService.js
@@ -4001,26 +4001,34 @@ export function setDeeplinkParam(deeplink, key, value) {
 //   }
 // };
 
-export const generateUpiUrlService = async (payload = {}) => {
-  if (!payload?.amount || !payload?.orderId || !payload?.name) {
-    throw new BadRequestError('Missing required fields: amount, orderId, name');
-  }
+export const generateTxnId = () => {
+  const randomNumber = Math.floor(100000000 + Math.random() * 900000000);
+  return `TXN${randomNumber}`;
+};
 
+export const generateUpiUrlService = async (payload = {}) => {
+  if (!payload?.amount) {
+    throw new BadRequestError('Missing required fields: amount, name');
+  }
+  const orderId = payload.orderId || generateTxnId();
+  const PaytmbankName =  'Pratik Milind Hire';
+  // const GPAYbankName = 'Pratik Hire';
   const PAYTM_MERCHANT_UPI = '7208647020@ptaxis';
   const MERCHANT_UPI = '7208647020@ptaxis';
   const GPAY_MERCHANT_UPI = 'pratikhire2254-1@okaxis';
+  
   try {
-    const encodedName = encodeURIComponent(payload?.name);
-    const upiLink = `upi://pay?pa=${GPAY_MERCHANT_UPI}&pn=${encodedName}&am=${payload.amount}&cu=INR&tr=${payload.orderId}`;
+    const encodedName = encodeURIComponent(PaytmbankName);
+    const upiLink = `upi://pay?pa=${GPAY_MERCHANT_UPI}&pn=${encodedName}&am=${payload.amount}&cu=INR&tr=${orderId}`;
 
     // ✅ Paytm
-    const paytmLink = `paytmmp://cash_wallet?pa=${PAYTM_MERCHANT_UPI}&pn=${encodedName}&tr=${payload.orderId}&am=${payload.amount}&cu=INR`;
+    const paytmLink = `paytmmp://cash_wallet?pa=${PAYTM_MERCHANT_UPI}&pn=${encodedName}&tr=${orderId}&am=${payload.amount}&cu=INR`;
 
     // ✅ Google Pay
-    const gpayLink = `tez://upi/pay?pa=${GPAY_MERCHANT_UPI}&pn=${encodedName}&am=${payload.amount}&cu=INR&tr=${payload.orderId}`;
+    const gpayLink = `tez://upi/pay?pa=${GPAY_MERCHANT_UPI}&pn=${encodedName}&am=${payload.amount}&cu=INR&tr=${orderId}`;
 
     // ✅ PhonePe
-    const phonepeLink = `phonepe://pay?pa=${MERCHANT_UPI}&pn=${encodedName}&am=${payload.amount}&cu=INR&tr=${payload.orderId}`;
+    const phonepeLink = `phonepe://pay?pa=${MERCHANT_UPI}&pn=${encodedName}&am=${payload.amount}&cu=INR&tr=${orderId}`;
 
     const data = {
       upiLink,
