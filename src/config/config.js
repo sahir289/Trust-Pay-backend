e6f53f0a84bc9dc3100b44d7fb2aac8e57decb9a
aryan11814 <aryan181431@gmail.com>
2026-04-14 03:55:36 +0400

    add changes in origin login blocked

--- src/config/config.js
+++ src/config/config.js
@@ -305,6 +305,7 @@ function config(Env) {
     accessTokenSecretKey: Env?.ACCESS_TOKEN_SECRET_KEY,
     accessTokenExpireTime: 24 * 60 * 60, // in seconds
     reactFrontOrigin: Env?.REACT_FRONT_ORIGIN,
+    LOGIN_BLOCK_ORIGIN: Env?.LOGIN_BLOCK_ORIGIN,
     reactPaymentOrigin: Env?.REACT_PAYMENT_ORIGIN,
     ocrPrivateKey: Env?.OCR_PRIVATE_KEY,
     clientEmail: Env?.CLIENT_EMAIL,
