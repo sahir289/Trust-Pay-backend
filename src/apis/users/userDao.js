33b25b60e61be1303895e78fc2a673be6a12415c
aryan11814 <aryan181431@gmail.com>
2026-04-17 17:34:48 +0400

    add changes in user login enabled and disabled

--- src/apis/users/userDao.js
+++ src/apis/users/userDao.js
@@ -610,6 +610,28 @@ const getUserByRoleDao = async (company_id, role, conn = null) => {
     throw error;
   }
 };
+const updateUserByIDDao = async (ids, data, conn = null) => {
+   try {
+     const { id } = ids;
+     const idArray = Array.isArray(id) ? id : [id];
+     const is_obsolete = data.is_obsolete;
+     const is_enabled = data.is_enabled;
+     const updated_by = data.updated_by;
+     const values = [is_obsolete, is_enabled, updated_by, idArray];
+     const sql = `
+      UPDATE "User"
+      SET "is_obsolete" = $1,
+          "is_enabled" = $2,
+          "updated_by" = $3
+      WHERE "id" = ANY($4)
+    `;
+     const result = await executeQuery(sql, values, conn);
+     return result.rows;
+   } catch (error) {
+     logger.error('Error in updateUserDao:', error.message);
+     throw error;
+   }
+};
 export {
   getUsersDao,
   getAllUsersDao,
@@ -623,4 +645,5 @@ export {
   updateUserDao,
   getUserDao,
   deleteUserDao,
+  updateUserByIDDao,
 };
