f08ad879a389583ce17e5c28cb2daf3e1bc5ce1f
Luv-83 <luvraghuvanshi83@gmail.com>
2026-04-21 10:00:26 +0400

    remove unwanted code

--- src/intent/createtytlPayIntentTransaction.js
+++ src/intent/createtytlPayIntentTransaction.js
@@ -3,77 +3,11 @@ import axios from 'axios';
 import { logger } from '../utils/logger.js';
 import config from '../config/config.js';
 
-/**
- * Generate SHA512 hash for a given provider
- * @param {Object} body - The transaction details (amount, order_id, etc.)
- * @param {Object} providerConfig - The provider config containing salt and collectionId.
- */
-// export function generateSign({ mId, mOrderId, amount, timestamp, secret }) {
-//   const raw = `${mId}${mOrderId}${amount}${timestamp}${secret}`;
-//   return crypto.createHash("md5").update(raw).digest("hex");
-// }
-
-function getSignString(params) {
-  // Clone object (avoid mutation)
-  const newParams = { ...params };
-
-  // Remove 'sign'
-  delete newParams.sign;
-
-  // Sort keys
-  const sortedKeys = Object.keys(newParams).sort();
-
-  const array = [];
-
-  for (const  key of sortedKeys) {
-      const value = newParams[key];
-
-      // PHP empty() equivalent check
-      if (
-          value !== undefined &&
-          value !== null &&
-          value !== "" &&
-          value !== 0 &&
-          value !== "0"
-      ) {
-          array.push(`${key}=${value}`);
-      }
-  }
-
-  return array.join("&");
-}
-
-export function generateSign(params, privateKey, algorithm = "RSA-SHA256") {
-    // Step 1: create sign string (you need same logic as PHP getSignString)
-    const signString = getSignString(params);
-
-    // Step 2: format private key
-    const formattedKey = `${privateKey}`;
-
-    // Step 3: create signer
-    const signer = crypto.createSign(algorithm);
-
-    signer.update(signString);
-    signer.end();
-
-    // Step 4: generate signature (base64)
-    const signature = signer.sign(formattedKey, "base64");
-
-    return signature;
-}
-/**
- * Create a payment transaction for a given provider.
- * @param {string} providerKey - Key from config (e.g. "zentechind" or "nmplPay")
- * @param {Object} deposit - Deposit object containing whole payin details
- * @param {number|string} amount - Transaction amount
- */
-
 export const createtytlPaymentTransaction = async (
   providerKey,
   deposit,
   amount
 ) => {
-  console.log(providerKey, "dsfdsdsdsdsdsds")
   try {
     const providerConfig = config[providerKey];
     if (!providerConfig) {
@@ -124,7 +58,6 @@ const headers = {
 
 // Send the request
 const response = await axios.post('https://api.tylt.money/p2pRampsMerchant/createInstance', raw, { headers })
-    console.log(response.data, "ssssswdedfedfedwd")
     logger.info(`${providerKey} transaction created:`, {
       requestBody,
       response: response?.data,
