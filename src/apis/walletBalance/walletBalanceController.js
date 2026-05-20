b5c0f045c96ab1281d397f0167d44d65a45cd3fe
mac <mac@macs-MacBook-Air.local>
2026-05-20 18:48:11 +0400

    add walletbalance api

---
+++ src/apis/walletBalance/walletBalanceController.js
@@ -0,0 +1,16 @@
+import { sendSuccess } from '../../utils/responseHandlers.js';
+import { getWalletBalanceService } from './walletBalanceService.js';
+import { BadRequestError } from '../../utils/appErrors.js';
+
+export const getWalletBalanceController = async (req, res) => {
+  const xApiKey = req.headers['x-api-key'];
+  const  code  = req.headers['code'];
+
+  if (!code) throw new BadRequestError('code is required');
+    if (!xApiKey) throw new BadRequestError('x-api-key is required');
+
+  const data = await getWalletBalanceService({ code, xApiKey });
+
+  return sendSuccess(res, data, 'Wallet balance fetched successfully');
+};
+
