e5595c269d230842ccbe101a47b8624226c926bd
Shadow <shadow77278837@gmail.com>
2026-07-24 16:29:17 +0400

    Revert "Bug Fix: 2FA related Bugs"
    This reverts commit 4fadda2b3e12647c534aeb6f2f6c568fa4a17dc5.

--- src/apis/users/userService.js
+++ src/apis/users/userService.js
@@ -783,9 +783,9 @@ const userUpdateService = async (ids, payload) => {
   }
 };
 
-const updateUser2FAService = async (id, isTwoFactorEnabled) => {
+const updateUser2FAService = async (id, isTwoFactorRequired) => {
   try {
-    return await updateUser2FAStatusDao(id, isTwoFactorEnabled);
+    return await updateUser2FAStatusDao(id, isTwoFactorRequired);
   } catch (error) {
     logger.error('Error in updateUser2FAService:', error);
     throw error;
