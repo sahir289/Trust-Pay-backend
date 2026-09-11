21c1a4fb021e7a81fead53203a49c106255d1e74
Luv-83 <luvraghuvanshi83@gmail.com>
2026-09-11 18:21:36 +0400

    add

--- src/middlewares/requestSignature.js
+++ src/middlewares/requestSignature.js
@@ -185,10 +185,6 @@ const verifyRequestSignature = (options = {}) => {
   //   timestamp,
   //   receivedSignature: signature,
   //   expectedSignature: expected,
-  //   secretUsedPrefix: secret ? secret.slice(0, 8) + '...' : null,
-  //   vendorId: req.vendor?.id,
-  //   banks: req.vendor?.banks?.map(b => ({ id: b.id, secretPrefix: b.secretKey?.slice(0, 8) })),
-  //   rawBodyLength: (req.rawBody || '').length,
   // });
       return sendError(res, 'Invalid request signature', 401, V2_ERROR_CODES.INVALID_SIGNATURE);
     }
